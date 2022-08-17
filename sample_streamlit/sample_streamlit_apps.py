297720dac152f5bb19c65603fdd261667aa69f1c
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-08-17 22:46:05 +0900

    sample_apps.py append sys.path

--- sample_streamlit/sample_streamlit_apps.py
+++ sample_streamlit/sample_streamlit_apps.py
@@ -4,8 +4,9 @@ import os, sys
 @st.experimental_singleton
 def installff():
   os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver/home/appuser/venv/bin/geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
   # os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver')
+  sys.path.append('/home/appuser/venv/bin/geckodriver')
 
 _ = installff()
 from selenium import webdriver

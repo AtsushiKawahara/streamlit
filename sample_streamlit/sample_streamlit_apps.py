494a26ac1c0f2bca4579ad8f5e190420ad89964b
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-08-20 11:10:08 +0900

    system add ls pwd

--- sample_streamlit/sample_streamlit_apps.py
+++ sample_streamlit/sample_streamlit_apps.py
@@ -5,6 +5,8 @@ import os, sys
 def installff():
   os.system('sbase install geckodriver')
   os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+  os.system('pwd')
+  os.system('ls')
   # os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver')
   sys.path.append('/home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox.exe')
   sys.path.append('/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver')

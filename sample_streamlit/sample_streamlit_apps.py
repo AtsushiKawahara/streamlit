a998ee97347f689717496d2244feaa1aa5bacbe0
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-08-20 12:22:44 +0900

    a

--- sample_streamlit/sample_streamlit_apps.py
+++ sample_streamlit/sample_streamlit_apps.py
@@ -5,13 +5,8 @@ import os, sys
 def installff():
   os.system('sbase install geckodriver')
   os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-  print('pwd-------------------------------------------')
-  print(os.system('pwd'))
-  print(os.system('ls'))
-  print('pwd-------------------------------------------')
   # os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver')
-  sys.path.append('/home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox.exe')
-  sys.path.append('/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver')
+  # sys.path.append('/home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox.exe')
   sys.path.append('/home/appuser/venv/bin/geckodriver')
 
 from selenium import webdriver
@@ -20,13 +15,12 @@ from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 _ = installff()
-options = Options()
-# options = FirefoxOptions()
-options.binary = FirefoxBinary(r'/home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox.exe')
-options.add_argument("--headless")
-# options.binary_location = r'/home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox'
-# options.binary_location = r'/home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox'
-driver = webdriver.Firefox(executable_path=r'/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver/geckodriver.exe', options=options)
+firefoxOptions = Options()
+firefoxOptions.add_argument("--headless")
+driver = webdriver.Firefox(
+    options=firefoxOptions,
+    executable_path="/home/appuser/venv/bin/geckodriver",
+)
 driver.get('http://google.com/')
 
 # memo----------------------------------------------

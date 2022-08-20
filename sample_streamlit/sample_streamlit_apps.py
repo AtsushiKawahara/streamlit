b29b8b06df27ab7a891d5fb3707c13c0b51b084e
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-08-20 10:46:02 +0900

    geckdriver.ext path append

--- sample_streamlit/sample_streamlit_apps.py
+++ sample_streamlit/sample_streamlit_apps.py
@@ -4,7 +4,7 @@ import os, sys
 @st.experimental_singleton
 def installff():
   os.system('sbase install geckodriver')
-  # os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
   # os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver')
   sys.path.append('/home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox.exe')
   sys.path.append('/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver')
@@ -15,7 +15,7 @@ from selenium.webdriver.firefox.options import Options
 
 options = Options()
 options.binary_location = r'/home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox.exe'
-driver = webdriver.Firefox(executable_path=r'/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver.exe', options=options)
+driver = webdriver.Firefox(executable_path=r'/home/appuser/venv/bin/geckodriver.exe', options=options)
 driver.get('http://google.com/')
 
 # memo----------------------------------------------

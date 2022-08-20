b18d7a89801991b69848de2991ff936b0f964cd2
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-08-20 10:43:29 +0900

    sample_streamlit edit https://stackoverflow.com/questions/65318382/expected-browser-binary-location-but-unable-to-find-binary-in-default-location

--- sample_streamlit/sample_streamlit_apps.py
+++ sample_streamlit/sample_streamlit_apps.py
@@ -4,16 +4,28 @@ import os, sys
 @st.experimental_singleton
 def installff():
   os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+  # os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
   # os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver')
+  sys.path.append('/home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox.exe')
+  sys.path.append('/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver')
   sys.path.append('/home/appuser/venv/bin/geckodriver')
 
-_ = installff()
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
+from selenium.webdriver.firefox.options import Options
 
-browser.get('http://example.com')
-st.write(browser.page_source)
+options = Options()
+options.binary_location = r'/home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox.exe'
+driver = webdriver.Firefox(executable_path=r'/home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/geckodriver.exe', options=options)
+driver.get('http://google.com/')
+
+# memo----------------------------------------------
+# from selenium import webdriver
+# from selenium.webdriver import FirefoxOptions
+# _ = installff()
+# opts = FirefoxOptions()
+# opts.add_argument("--headless")
+# browser = webdriver.Firefox(options=opts)
+#
+# browser.get('http://example.com')
+# st.write(browser.page_source)
+# memo----------------------------------------------

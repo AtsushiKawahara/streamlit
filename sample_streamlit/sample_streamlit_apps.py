19cb449d4d2c31d987d60fd31c3062e5cfb41cdc
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-08-21 20:35:21 +0900

    add ls and pwd

--- sample_streamlit/sample_streamlit_apps.py
+++ sample_streamlit/sample_streamlit_apps.py
@@ -90,6 +90,8 @@ from selenium.webdriver.common.by import By
 # st.write(os.system("find /home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver/firefox"))
 # os.system("grep -r .exe /home/")
 # os.system("find ./ -name `firefox.exe`")
+os.system("pwd")
+os.system("ls ./")
 os.system("find ./ -name `chrom`")
 # st.write(os.system("ls /home/appuser/venv/lib/python3.9/site-packages/selenium/webdriver"))
 options = Options()

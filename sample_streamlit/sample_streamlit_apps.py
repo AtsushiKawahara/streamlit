79226cdcfa51b6c9deea64aa2f3851e05b434f2d
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-08-24 23:32:17 +0900

    a

--- sample_streamlit/sample_streamlit_apps.py
+++ sample_streamlit/sample_streamlit_apps.py
@@ -94,8 +94,9 @@ from selenium.webdriver.common.by import By
 # os.system("ls /home/appuser/venv/lib/python3.9/site-packages/chromedriver_binary")
 # os.system("ls /home/appuser/venv/lib/python3.9/site-packages")
 # os.system("find /home/appuser/venv/lib/python3.9/site-packages -type f")
-os.system("find /home/appuser/venv/lib/python3.9/ -name `chrome*` -type f")
-os.system("find /home/appuser/venv/lib/python3.9/ -name `chrome*` -type d")
+os.system("find /home/venv -type f")
+# os.system("find /home/appuser/venv/lib/python3.9/ -name `chrome*` -type f")
+# os.system("find /home/appuser/venv/lib/python3.9/ -name `chrome*` -type d")
 # os.system("find /home/appuser/venv/lib/python3.9/ -type f")
 # os.system("find ./ -name `firefox.exe`")
 # os.system("pwd")  # /app/streamlit

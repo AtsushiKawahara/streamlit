e23c5f420fe64f7875ae2e3da11223a874a15f48
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-09-13 22:06:33 +0900

    不要なパッケージの削除

--- streamlit/apps/scraping_shutuba_table.py
+++ streamlit/apps/scraping_shutuba_table.py
@@ -8,37 +8,13 @@
 import pandas as pd
 import time
 from tqdm import tqdm
-# import sys
 import re
-# from scipy.special import comb
-# import requests
-# from bs4 import BeautifulSoup
 from urllib.request import urlopen
-# import numpy as np
-# # XGBoost
-# import xgboost as xgb
-# from sklearn.metrics import accuracy_score, confusion_matrix
-# # ROC曲線を作成する
-# from sklearn.metrics import roc_curve, roc_auc_score
-# import matplotlib.pyplot as plt
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.preprocessing import MinMaxScaler
-# import math
-# from sklearn.preprocessing import LabelEncoder
 from selenium.webdriver import Chrome, ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.core.utils import ChromeType
-# # 組み合わせ馬券の計算に使用
-# from scipy.special import comb, perm  # nCr nPr
-# import itertools
-# # pycaretライブラリのインポート
-# from pycaret.datasets import get_data
-# from pycaret.regression import *
-# from pycaret.classification import *
-# # selenium関係のライブラリのインポート
-# from selenium import webdriver
 # streamlit用に必要なライブラリのインポート
 import sys
 import os
@@ -58,9 +34,6 @@ PLACE_DICT_REVERSE = {
 RACE_TYPE_DICT = {
     "ダ": "ダート", "芝": "芝", "障": "障害"
     }
-
-# 競馬サイトのurl（メモとしておいとくだけ）
-# url = "https://db.netkeiba.com/race/201901010101/"
 
 # 学習データとして使用する年を設定
 GET_DATA_YEAR_LIST = [2017, 2018, 2019, 2020, 2021, 2022]  # 取得したい年を指定
@@ -92,19 +65,8 @@ print('/'.join(os.path.abspath(__file__).split('/')[:-2])+'/')
 # sys.path.append(FILE_PATH_TMP)
 
 # 自作関数のインポート
-# from data_proessing import load_pickle
-# from data_proessing import save_pickle
-# from data_proessing import re_specific_text_get
-# from data_proessing import train_test_split
-# from data_proessing import roc_graph_plot
 from apps.predict import Preprocessing_Horse_Result  # 不要
-# from predict import Calucurate_Return
-# from predict import Return
 from apps.predict import Data_Processer
-# from predict import Result_Table
-# from predict import labelencoder_ped
-# from horse_ped_get import scrape_horse_ped
-# from horse_result_get import scrape_horse_result
 
 
 class Start_Horse_Table(Data_Processer):
@@ -134,18 +96,6 @@ class Start_Horse_Table(Data_Processer):
         # 日付を入力するとその日のレース情報(説明変数情報)を取得できるようにする
 
         url = "https://race.netkeiba.com/top/"
-
-        # memo-----------------------------------------------------------------
-        # firefoxを使用する場合のコード
-        # firefoxOptions = Options()
-        # firefoxOptions.add_argument("--headless")
-        # service = Service(GeckoDriverManager().install())
-        # driver = webdriver.Firefox(
-        # options=firefoxOptions,
-        # service=service,
-        # )
-        # driver.get(url)
-        # memo-----------------------------------------------------------------
 
         # googleを起動
         options = ChromeOptions()  # ここで拡張機能を本来は設定するけど今回は省略
@@ -436,217 +386,3 @@ class Start_Horse_Table(Data_Processer):
         # df['n_horses'] = df.index.map(df.index.value_counts())
 
         self.data_p = df
-
-
-def main():
-    # main()関数を実行するために必要な関数をimportしておく
-    from functions.data_proessing import save_pickle
-
-    # 1.学習データの読み込み-------------------------------------------------------
-
-    # 学習時に使用したデータを用意(ラベルエンコーディング等をするときに必要なため)
-    # 馬ごとの成績データの処理(過去データの着順・賞金の平均を説明変数にするために使用する)
-    hr = Preprocessing_Horse_Result.load_pickle(GET_DATA_YEAR_LIST)
-
-    # 2.出馬テーブルを取得---------------------------------------------------------
-
-    # memo---------------------------------------------------------------------
-    # 方法１
-    # race_id_list = [f"2022100107{str(i).zfill(2)}" for i in range(1, 2, 1)]
-    # race_id_list = re.findall(r"\d+", url)
-
-    # url = "https://race.netkeiba.com/race/result.html?race_id=202210030401&rf=race_list"
-    # race_id_list = re.findall(r"\d+", url)
-
-    # url = f"https://race.netkeiba.com/race/shutuba.html?race_id={race_id}&rf=race_list"
-    # race_id = 202201010110
-    # race_id = 202201010301
-    # memo---------------------------------------------------------------------
-
-    # 取得したいレース日を指定してスクレイピングする
-    target_date = "8月7日"  # ○月○日の形で出馬テーブルを取得したい日を指定する
-    save_pickle(FILE_PATH_TMP, "target_date.pickle", target_date)  # streamlit用に保存しておく
-
-    # 出馬テーブルのスクレイピング(target_dateにより取得するレースの日付を指定する方法)
-    sht = Start_Horse_Table()  # 予測したいレースのrace_id_listを渡す
-    sht.scrape_by_ChromeDriverManager_at_target_date(target_date, is_real_time=True, table_type="shutuba_table")  # 予測したいレースのrace_id_listを渡す
-
-    # memo---------------------------------------------------------------------
-    # 出馬テーブルのスクレイピング(race_id_listを作成して出馬テーブルを取得する方法)
-    # sht = Start_Horse_Table()  # 予測したいレースのrace_id_listを渡す
-    # st.scrape_by_ChromeDriverManager_at_race_id_list(race_id_list)  # 予測したいレースのrace_id_listを渡す
-    # memo---------------------------------------------------------------------
-
-    save_pickle(FILE_PATH_TMP, "race_info_dict.pickle", sht.race_info_dict)  # レースごとのレース名、出走時刻などのデータdictを保存しておく(key: race_id)
-    save_pickle(FILE_PATH_TMP, "shutuba_tables.pickle", sht.shutuba_tables)  # レースごとのレース名、出走時刻などのデータdictを保存しておく(key: race_id)
-
-    # memo---------------------------------------------------------------------
-    # sht.data  # preprocessing前
-    # sht.race_info_dict
-    # sht.shutuba_tables
-    # memo---------------------------------------------------------------------
-
-    # 3.出馬テーブルの加工(データ型の変更やデータの分割など)------------------------------
-    sht.preprocessing_shutuba_table()
-
-    # memo---------------------------------------------------------------------
-    # sht.data_p  # preprocessing後
-    # memo---------------------------------------------------------------------
-
-    # 3-1.過去成績データを追加-----------------------------------------------------
-
-    # データベースに過去成績データが存在しない馬のhorse_idを取得
-    not_exist_horse_results_horse_id_list = sht.data_p[~sht.data_p["horse_id"].isin(hr.pd_horse_results.index)]["horse_id"]
-
-    if len(not_exist_horse_results_horse_id_list) != 0:
-
-        # 過去成績データがないhorse_idの過去成績データをスクレイピングする
-        pd_horse_results_dict_add = scrape_horse_result(not_exist_horse_results_horse_id_list)
-
-        # 馬ごとのレース成績データをレースIDに変更
-        for key in pd_horse_results_dict_add.keys():
-            pd_horse_results_dict_add[key].index = [key]*len(pd_horse_results_dict_add[key])
-
-        if len(pd_horse_results_dict_add) > 0:
-            # スクレイピングしてきた過去成績データをhorse_idをindexにして結合する(dict → dataframe)
-            pd_horse_results_add = pd.concat([pd_horse_results_dict_add[key] for key in pd_horse_results_dict_add.keys()], axis=0)
-
-            # 新たに取得した過去成績データを加工してデータベースと同じ形状にする
-            hr_add = Preprocessing_Horse_Result(pd_horse_results_add)
-            hr_add.pd_horse_results.shape  # 新たに取得した過去成績データ
-
-            # データベースに新たにスクレイピングした過去成績データを結合する
-            hr.pd_horse_results = pd.concat([hr.pd_horse_results, hr_add.pd_horse_results], axis=0)
-        else:
-            # 新たに取得できたデータがない場合(ネット上にもその馬の過去成績データが存在していない場合→まだ勝ったことがない馬等)
-            pass
-
-    # memo---------------------------------------------------------------------
-    # hr_add.pd_horse_results.shape
-    # hr.pd_horse_results.shape
-    # hr.pd_horse_results.shape
-    # memo---------------------------------------------------------------------
-
-    sht.merge_n_samples(hr)  # 過去成績データからレースの賞金データを出馬表に追加する
-
-    # memo---------------------------------------------------------------------
-    # sht.data_r  # 賞金データを追加
-    # memo---------------------------------------------------------------------
-
-    # 3-2.血統データを追加---------------------------------------------------------
-    # 血統データをlabelencodeするためのclassをインスタンス化
-    la = labelencoder_ped.load_pickle(GET_DATA_YEAR_LIST)
-
-    # memo---------------------------------------------------------------------
-    # la.pd_ped_datas
-    # la.pd_ped_datas.shape
-    # memo---------------------------------------------------------------------
-
-    # 重複して取得してしまっている血統データを削除する(年度ごとに血統データを取得すると重複データが存在するため)
-    la.pd_ped_datas["horse_id"] = la.pd_ped_datas.index  # horse_idが同一の行を削除するため一時的にcolumnにhorse_idを加える
-
-    # memo---------------------------------------------------------------------
-    # la.pd_ped_datas.shape
-    # memo---------------------------------------------------------------------
-
-    # horse_idが同一データの行を削除する(この処理をするためにhorse_idをcolumnに加える)
-    # subsetに指定したcolumnで重複行を探す
-    # keep:重複した場合にlastの行を削除する
-    la.pd_ped_datas = la.pd_ped_datas.drop_duplicates(subset=["horse_id"], keep="last")
-    la.pd_ped_datas.drop(["horse_id"], axis=1, inplace=True)
-
-    # 血統データがデータベースに存在しない馬のhorse_idを取得
-    not_exist_pd_ped_datas_horse_id_list = sht.data_r[~sht.data_r["horse_id"].isin(la.pd_ped_datas.index)]["horse_id"]
-
-    if len(not_exist_pd_ped_datas_horse_id_list) != 0:
-        # 血統データがないhorse_idの血統データをスクレイピングする
-        pd_ped_datas_dict_add = scrape_horse_ped(not_exist_pd_ped_datas_horse_id_list)
-        pd_ped_datas_add = pd.concat([pd_ped_datas_dict_add[key] for key in pd_ped_datas_dict_add.keys()], axis=1).T  # スクレイピングしてきたデータをhorse_idをindexにして結合する(dict → dataframe)
-        pd_ped_datas_add = pd_ped_datas_add.add_prefix("ped_")
-
-        # 血統データベースに新たにスクレイピングした血統データを結合する
-        la.pd_ped_datas = pd.concat([la.pd_ped_datas, pd_ped_datas_add], axis=0)
-
-    la.labelencode_ped()  # 血統データをラベルエンコーディング
-
-    sht.merge_ped_data(la.pd_ped_datas_la)  # pd_ped_datas_laは血統データのデータベースをラベルエンコディングしたやつ
-
-    # memo---------------------------------------------------------------------
-    # sht.data_ped  # 血統データを追加
-    # sht.data_ped.shape
-    # memo---------------------------------------------------------------------
-
-    # 必要な行も削除している可能性があるため確認しておく
-    if len(sht.data_ped) == len(sht.data_r):
-        print("不要行削除完了(必要な行は削除していない)")
-    else:
-        raise Exception("please check sht.data_ped len")
-
-    # 3-3.ラベルエンコーディング-----------------------------------------------------
-
-    # 学習したときのエンコーディングデータを読み込む(同一データは同一ラベルに変換するため)
-    le_horse_id = load_pickle(FILE_PATH_TMP, "le_horse_id.pickle")
-    le_jockey_id = load_pickle(FILE_PATH_TMP, "le_jockey_id.pickle")
-    le_trainer_id = load_pickle(FILE_PATH_TMP, "le_trainer_id.pickle")
-    data_ped = load_pickle(FILE_PATH_TMP, "data_ped.pickle")
-
-    # ラベルエンコーディングを実行
-    sht.labelencoder_id(le_horse_id, le_jockey_id, le_trainer_id, data_ped)
-
-    # 3-4.説明変数の形状を整える(不要なデータは削除)
-    X = sht.data_id.drop(["date"], axis=1)
-    X_add_tansho_ninnki = X.copy()  # Calucurate_Return classで"うまい馬券"の予測テーブルを作成するために"単勝, 人気"データが必要なため"X_add_tansho_ninnki"変数を作成しておく
-    X.drop(["単勝", "人気"], axis=1, inplace=True)
-
-    # memo---------------------------------------------------------------------
-    # 出馬テーブル一覧
-    # sht.data  # preprocessing前
-    # sht.data_p  # preprocessing後
-    # sht.data_r  # 賞金データを追加
-    # sht.data_ped  # 血統データを追加
-    # sht.data_id  # horse_idなどをラベルエンコーディングして追加
-    # X  # 加工完了モデル入力データ
-    # X.info()
-    # memo---------------------------------------------------------------------
-
-    # 4.予測--------------------------------------------------------------------
-
-    # 学習済みのモデルを読み込み
-    model = load_pickle(FILE_PATH_TMP, "lgb_clf.pickle")
-
-    # 説明変数の形状の確認のために、学習時の訓練データを読み込み
-    X_train = load_pickle(FILE_PATH_TMP, "X_train.pickle")
-    print(f"モデル学習時データとのcolumn数の違い:{len(set(X_train.columns) - set(X.columns))}")  # columns数が異なる場合のcheck
-
-    # 三連単・三連複の予測テーブルの作成
-    cr_sann = Calucurate_Return(model, GET_DATA_YEAR_LIST, X_add_tansho_ninnki, X, "sannrenntann_and_sannrennpuku", is_standard_scarer=False, is_use_pycaret=False)
-    save_pickle(FILE_PATH_TMP, "predict_table_sannrenntann.pickle", cr_sann.pred_table_sannrenntann)  # 三連単予測テーブル保存
-    save_pickle(FILE_PATH_TMP, "predict_table_sannrennpuku.pickle", cr_sann.pred_table_sannrennpuku)  # 三連複予測テーブル保存
-
-    # 単勝・複勝の予測テーブルの作成
-    cr_single = Calucurate_Return(model, GET_DATA_YEAR_LIST, X_add_tansho_ninnki, X, "single_and_fukushou", is_standard_scarer=False, is_use_pycaret=False)
-    save_pickle(FILE_PATH_TMP, "predict_table.pickle", cr_single.pred_table)  # 単勝・複勝(単勝と複勝は同じ予測テーブル)予測テーブル保存
-
-    # 馬単・馬蓮の予測テーブルの作成
-    cr_two = Calucurate_Return(model, GET_DATA_YEAR_LIST, X_add_tansho_ninnki, X, "umatan_and_umaren", is_standard_scarer=False, is_use_pycaret=False)
-    save_pickle(FILE_PATH_TMP, "predict_table_umatan.pickle", cr_two.pred_table_umatan)  # 馬単予測テーブル保存
-    save_pickle(FILE_PATH_TMP, "predict_table_umaren.pickle", cr_two.pred_table_umaren)  # 馬連予測テーブル保存
-
-    # 回収率算出
-    target_date_reuslt = "7月31日"
-    sht = Start_Horse_Table()
-    sht.scrape_by_ChromeDriverManager_at_target_date(target_date_reuslt, is_real_time=False, table_type="shutuba_table")  # レース結果を取得したいrace_id_listを渡す
-    sht.scrape_by_ChromeDriverManager_at_target_date(target_date_reuslt, is_real_time=False, table_type="result_table")  # レース結果を取得したいrace_id_listを渡す
-    sht.result_tables
-    sht.data  # 説明変数作成用
-    sht.race_info_dict
-    sht.shutuba_tables  # streamlitで表示用の出馬テーブル
-    sht.result_tables  # 回収率算出用のresultテーブル
-
-    cr_sann = Calucurate_Return(model, GET_DATA_YEAR_LIST, X_add_tansho_ninnki, X, "sannrenntann_and_sannrennpuku", is_standard_scarer=False, is_use_pycaret=False)
-
-
-
-
-if __name__ == '__main__':
-    main()

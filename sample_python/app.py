72c8a993ad5a2bfab1d81d0081ce01b80d7e989e
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-10-15 23:51:38 +0900

    sample-python edit

--- sample_python/app.py
+++ sample_python/app.py
@@ -1,18 +1,10 @@
 # coding:utf-8
 
 # 必要なライブラリのimport
-import pandas as pd
-import numpy as np
+import urllib3
+import json
 
+http = urllib3.PoolManager()
+r = http.request('GET', 'https://example.com/')
 
-def main():
-    # 適当にdfを生成
-    df = pd.DataFrame(np.arange(12).reshape(3, 4))
-
-    # 作成したdfを表示
-    print(df)
-    print(df)
-
-
-if __name__ == '__main__':
-    main()
+print(json.dumps(dict(r.headers), ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': ')))

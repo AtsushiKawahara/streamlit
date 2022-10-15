cc11bde84213b6bc4584e890bd50f766eea0cac9
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-10-15 23:55:44 +0900

    sample_python edit

--- sample_python/app.py
+++ sample_python/app.py
@@ -1,10 +1,11 @@
 # coding:utf-8
 
 # 必要なライブラリのimport
+import streamlit as st
 import urllib3
 import json
 
 http = urllib3.PoolManager()
 r = http.request('GET', 'https://example.com/')
 
-print(json.dumps(dict(r.headers), ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': ')))
+st.write(json.dumps(dict(r.headers), ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': ')))

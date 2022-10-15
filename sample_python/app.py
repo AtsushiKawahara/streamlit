2b328f51239abc54da83d19183bac0df73004e25
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-10-16 00:36:38 +0900

    sample-python edit

--- sample_python/app.py
+++ sample_python/app.py
@@ -4,8 +4,22 @@
 import streamlit as st
 import urllib3
 import json
+from selenium.webdriver import Chrome, ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
 
-http = urllib3.PoolManager()
-r = http.request('GET', 'https://example.com/')
+press_button = st.button("出馬テーブル取得開始")
 
-st.write(json.dumps(dict(r.headers), ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': ')))
+# ボタンが押されたときに実行される箇所
+if press_button:
+    url = 'https://example.com/'
+    http = urllib3.PoolManager()
+    r = http.request('GET', url)
+    st.write(json.dumps(dict(r.headers), ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': ')))
+
+    url = "https://race.netkeiba.com/top/"
+    options = ChromeOptions()  # ここで拡張機能を本来は設定するけど今回は省略
+    options.add_argument("--headless")
+    # driver = webdriver.Remote(command_executor="http://selenium:4444/wd/hub", options=options)
+    driver = Chrome(ChromeDriverManager().install(), options=options)
+    driver.get(url)
+    driver.close()

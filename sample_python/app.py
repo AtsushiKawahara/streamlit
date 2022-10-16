211f4337672d2d92371d3ed93bb854ccaa721315
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-10-16 21:13:23 +0900

    add requests

--- sample_python/app.py
+++ sample_python/app.py
@@ -7,25 +7,33 @@ import json
 from selenium.webdriver import Chrome, ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+import requests
 
 press_button = st.button("出馬テーブル取得開始")
 
 # ボタンが押されたときに実行される箇所
 if press_button:
     url = 'https://example.com/'
-    url = "https://race.netkeiba.com/top/"
+    # url = "https://race.netkeiba.com/top/"
 
     # urllib3によるサーバーへのhttpリクエスト
+    st.write("urllib3によるhttpリクエスト")
     http = urllib3.PoolManager()
     r = http.request('GET', url)
     st.write(r.status)
     st.write(json.dumps(dict(r.headers), ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': ')))
     st.write(r.data.decode('ascii', errors="ignore"))
 
+    # request(サードパーティー)によるhttpリクエスト
+    st.write("requestによるhttpリクエスト")
+    response = requests.get(url)
+    st.write(response)
+    st.write(response.text)
+
     # seleniumnによる通信
-    options = ChromeOptions()  # ここで拡張機能を本来は設定するけど今回は省略
-    options.add_argument("--headless")
-    driver = webdriver.Remote(command_executor="http://127.0.0.1:4444/wd/hub", options=options)
-    # driver = Chrome(ChromeDriverManager().install(), options=options)
-    driver.get(url)
-    driver.close()
+    # options = ChromeOptions()  # ここで拡張機能を本来は設定するけど今回は省略
+    # options.add_argument("--headless")
+    # driver = webdriver.Remote(command_executor="http://127.0.0.1:4444/wd/hub", options=options)
+    # # driver = Chrome(ChromeDriverManager().install(), options=options)
+    # driver.get(url)
+    # driver.close()

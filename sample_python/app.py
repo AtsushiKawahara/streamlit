60150ff215d7c164c4b26b44f3fa42c18ad889d9
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-10-19 22:36:55 +0900

    a

--- sample_python/app.py
+++ sample_python/app.py
@@ -54,7 +54,7 @@ if press_button:
     # chrome_driver_path = "./benchmark-auto/chromedriver.exe"
     # chrome_driver_path = "../.././app/streamlit/benchmark-auto/chromedriver.exe"
     # chrome_driver_path = os.getcwd() + "\chromedriver.exe"
-    chrome_driver_path = "\app\streamlit\benchmark-auto\chromedriver.exe"
+    chrome_driver_path = "app\streamlit\benchmark-auto\chromedriver.exe"
     options = ChromeOptions()  # ここで拡張機能を本来は設定するけど今回は省略
     options.add_argument("--headless")
     # driver = webdriver.Remote(command_executor="http://127.0.0.1:4444/wd/hub", options=options)

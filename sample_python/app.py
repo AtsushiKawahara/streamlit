f18698cac0c5d6fac7a920dd7c26ccf477512074
atsushi kawahara <KawaharaAtushi0916@gmail.com>
2022-10-23 17:32:02 +0900

    a

--- sample_python/app.py
+++ sample_python/app.py
@@ -14,16 +14,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome import service as fs
 
-# faniy
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.firefox import GeckoDriverManager
-
 st.write("pathの確認")
 # print(f"pwd:{os.system('pwd')}")
 # print(f"cd ~ :{os.system('cd ..')}")
@@ -35,22 +25,6 @@ print(os.system('find ../../. -type f -name "*"'))
 press_button = st.button("出馬テーブル取得開始")
 # ボタンが押されたときに実行される箇所
 if press_button:
-
-    URL = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
-    XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
-    TIMEOUT = 20
-
-    st.title("Test Selenium")
-    st.markdown("You should see some random Football match text below in about 21 seconds")
-
-    firefoxOptions = Options()
-    firefoxOptions.add_argument("--headless")
-    service = Service(GeckoDriverManager().install())
-    driver = webdriver.Firefox(
-        options=firefoxOptions,
-        service=service,
-        )
-    driver.get(URL)
 
     url = 'https://example.com/'
     # url = "https://race.netkeiba.com/top/"
